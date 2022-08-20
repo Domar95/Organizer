fb246b9604499cc85c4b7afef986de364aea314c
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2022-08-20 18:08:46 +0200

    update db

--- src/mysql_db/db_manager.py
+++ src/mysql_db/db_manager.py
@@ -33,6 +33,11 @@ class DBManager():
         self.mycursor.execute(
             f"'CREATE TABLE testz (testID int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), text VARCHAR(100), generalCategory VARCHAR(50), importance smallint UNSIGNED, date VARCHAR(20), duration smallint UNSIGNED)'")
 
+    # function specific for this app rather than for db manager, can be done with args kwargs for db, below one is for this app
+    # def insert_into_db(self, table_name, record_name=None, record_text=None, record_general_category=None, record_importance=None, record_date=None, duration=None, deadline=None, domain=None, link=None, image=None, used_technologies=None):
+    #    self.mycursor.execute(f"'INSERT INTO DailyGoal (name, text, generalCategory, importance, date, duration) VALUES (%s,%s,%s,%s,%s,%s)",
+#   #              ('workout', '3 times a week gym workout', 'object (reference general-cat obj)/another table', 8, '07.08.2022', 90))
+
 
 if __name__ == '__main__':
     db_instance = DBManager()
